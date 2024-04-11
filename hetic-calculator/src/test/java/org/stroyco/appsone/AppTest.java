65c46bb9b35412a2f274623b56cd19e82e999ac4
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2024-04-11 11:48:58 +0200

    feat: add some tests

--- hetic-calculator/src/test/java/org/stroyco/appsone/AppTest.java
+++ hetic-calculator/src/test/java/org/stroyco/appsone/AppTest.java
@@ -1,38 +1,46 @@
 package org.stroyco.appsone;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.stroyco.appsone.Factory.OperationFactory;
+import org.stroyco.appsone.Strategy.Operation;
+import org.stroyco.appsone.Strategy.Implementation.AddOperation;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    private final Operation addition = OperationFactory.getOperation("+");
+    private final Operation subtraction = OperationFactory.getOperation("-");
+    private final Operation multiplication = OperationFactory.getOperation("*");
+    private final Operation division = OperationFactory.getOperation("/");
+
+    @Test
+    public void getOperation() {
+        Operation operation = OperationFactory.getOperation("+");
+        assertNotNull(operation);
+        assertTrue(operation instanceof AddOperation);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testAddition() {
+        assertEquals(5, addition.perform(2, 3));
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testSubtraction() {
+        assertEquals(2, subtraction.perform(5, 3));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(6, multiplication.perform(2, 3));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(2, division.perform(6, 3));
     }
 }
